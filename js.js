bde54c9de4d8481fd8787fd9c3211e652f7ea695
Pedro <465.morales.pedro@gmail.com>
2023-05-12 17:52:56 -0500

    password and password confirm must match in order to send the form now

--- js.js
+++ js.js
@@ -1,14 +1,16 @@
 const password = document.querySelector("#password");
 const passwordConfirm = document.querySelector("#password-confirm");
-const passwordconfirmerr = document.querySelector("#passwordconfirmerr");
+// const passwordconfirmerr = document.querySelector("#passwordconfirmerr");
 
 console.log(password); // check if element is found
 console.log(passwordConfirm); // check if element is found
 function checkPasswords() {
   if (password.value !== passwordConfirm.value) {
-    passwordconfirmerr.textContent = "passwords must match";
+    passwordConfirm.setCustomValidity("passwords must match");
+  } else {
+    passwordConfirm.setCustomValidity("");
   }
 }
 
 const form = document.querySelector("form");
-form.addEventListener("submit", checkPasswords);
+passwordConfirm.addEventListener("input", checkPasswords);
